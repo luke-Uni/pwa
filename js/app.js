2113624ffb0d4eee188678bc3220f1b3a5a19116
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-11-06 18:59:52 +0100

    add service worker

--- js/app.js
+++ js/app.js
@@ -124,3 +124,12 @@
   // once loading is complete.
   window.addEventListener("load", startup, false);
 })();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
