5cec0c46d38bcfbc4758a5c194be705b62b494db
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-11-10 16:35:17 +0100

    audio and video

--- js/app.js
+++ js/app.js
@@ -1,129 +1,144 @@
-(() => {
-  // The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
 
-  const width = 320; // We will scale the photo width to this
-  let height = 0; // This will be computed based on the input stream
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
 
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function (resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
+  }
+}
 
-  let streaming = false;
+function getStream(type) {
+  if (
+    !navigator.mediaDevices &&
+    !navigator.getUserMedia &&
+    !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia &&
+    !navigator.msGetUserMedia
+  ) {
+    alert("User Media API not supported.");
+    return;
+  }
 
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
+  var constraints = {};
+  constraints[type] = true;
 
-  let video = null;
-  let canvas = null;
-  let photo = null;
-  let startbutton = null;
+  getUserMedia(constraints)
+    .then(function (stream) {
+      var mediaControl = document.querySelector(type);
 
-  function showViewLiveResultButton() {
-    if (window.self !== window.top) {
-      // Ensure that if our document is in a frame, we get the user
-      // to first open it in its own tab or window. Otherwise, it
-      // won't be able to request permission for camera access.
-      document.querySelector(".contentarea").remove();
-      const button = document.createElement("button");
-      button.textContent = "View live result of the example code above";
-      document.body.append(button);
-      button.addEventListener("click", () => window.open(location.href));
-      return true;
+      if ("srcObject" in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(
+          stream
+        );
+      }
+
+      mediaControl.play();
+    })
+    .catch(function (err) {
+      alert("Error: " + err);
+    });
+}
+
+function getUserMedia(options, successCallback, failureCallback) {
+  var api =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+  if (api) {
+    return api.bind(navigator)(options, successCallback, failureCallback);
+  }
+}
+
+var theStream;
+var theRecorder;
+var recordedChunks = [];
+
+function getStream() {
+  if (
+    !navigator.getUserMedia &&
+    !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia &&
+    !navigator.msGetUserMedia
+  ) {
+    alert("User Media API not supported.");
+    return;
+  }
+
+  var constraints = { video: true, audio: true };
+  getUserMedia(
+    constraints,
+    function (stream) {
+      var mediaControl = document.querySelector("video");
+
+      if ("srcObject" in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(
+          stream
+        );
+      }
+
+      theStream = stream;
+      try {
+        recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+      } catch (e) {
+        console.error("Exception while creating MediaRecorder: " + e);
+        return;
+      }
+      theRecorder = recorder;
+      console.log("MediaRecorder created");
+      recorder.ondataavailable = recorderOnDataAvailable;
+      recorder.start(100);
+    },
+    function (err) {
+      alert("Error: " + err);
     }
-    return false;
-  }
+  );
+}
 
-  function startup() {
-    if (showViewLiveResultButton()) {
-      return;
-    }
-    video = document.getElementById("video");
-    canvas = document.getElementById("canvas");
-    photo = document.getElementById("photo");
-    startbutton = document.getElementById("startbutton");
+function recorderOnDataAvailable(event) {
+  if (event.data.size == 0) return;
+  recordedChunks.push(event.data);
+}
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(`An error occurred: ${err}`);
-      });
+function download() {
+  console.log("Saving data");
+  theRecorder.stop();
+  theStream.getTracks()[0].stop();
 
-    video.addEventListener(
-      "canplay",
-      (ev) => {
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width);
+  var blob = new Blob(recordedChunks, { type: "video/webm" });
+  var url = (window.URL || window.webkitURL).createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "test.webm";
+  a.click();
 
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
-
-          if (isNaN(height)) {
-            height = width / (4 / 3);
-          }
-
-          video.setAttribute("width", width);
-          video.setAttribute("height", height);
-          canvas.setAttribute("width", width);
-          canvas.setAttribute("height", height);
-          streaming = true;
-        }
-      },
-      false
-    );
-
-    startbutton.addEventListener(
-      "click",
-      (ev) => {
-        takepicture();
-        ev.preventDefault();
-      },
-      false
-    );
-
-    clearphoto();
-  }
-
-  // Fill the photo with an indication that none has been
-  // captured.
-
-  function clearphoto() {
-    const context = canvas.getContext("2d");
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    const data = canvas.toDataURL("image/png");
-    photo.setAttribute("src", data);
-  }
-
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
-
-  function takepicture() {
-    const context = canvas.getContext("2d");
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-
-      const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-    } else {
-      clearphoto();
-    }
-  }
-
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  window.addEventListener("load", startup, false);
-})();
+  // setTimeout() here is needed for Firefox.
+  setTimeout(function () {
+    (window.URL || window.webkitURL).revokeObjectURL(url);
+  }, 100);
+}
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
