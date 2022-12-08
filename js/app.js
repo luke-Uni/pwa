49399d1516c9be96d0e532ab60387d45beda0533
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2022-12-08 16:07:07 +0100

    local notification

--- js/app.js
+++ js/app.js
@@ -389,12 +389,59 @@ async function writeFile() {
   await writer.write(0, "Hello world from What Web Can Do!");
   await writer.close();
 }
-target.innerHTML = "Test content written to " + file.name + ".";
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
+
+var $status = document.getElementById("status");
+
+if ("Notification" in window) {
+  $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+  if (!("Notification" in window)) {
+    alert("Notification API not supported!");
+    return;
+  }
+
+  Notification.requestPermission(function (result) {
+    $status.innerText = result;
   });
 }
+
+function nonPersistentNotification() {
+  if (!("Notification" in window)) {
+    alert("Notification API not supported!");
+    return;
+  }
+
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+    console.log("nonPersistentNotification sent");
+  } catch (err) {
+    alert("Notification API error: " + err);
+  }
+}
+
+function persistentNotification() {
+  if (!("Notification" in window) || !("ServiceWorkerRegistration" in window)) {
+    alert("Persistent Notification API not supported!");
+    return;
+  }
+
+  try {
+    navigator.serviceWorker
+      .getRegistration()
+      .then((reg) => reg.showNotification("Hi there - persistent!"))
+      .catch((err) => alert("Service Worker registration error: " + err));
+  } catch (err) {
+    alert("Notification API error: " + err);
+  }
+}
+//target.innerHTML = "Test content written to " + file.name + ".";
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function () {
+//     navigator.serviceWorker
+//       .register("/serviceWorker.js")
+//       .then((res) => console.log("service worker registered"))
+//       .catch((err) => console.log("service worker not registered", err));
+//   });
+// }
