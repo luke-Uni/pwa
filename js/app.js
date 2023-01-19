7434ab1dc6dad6b11c9c04693291b8df7abb52ad
luke-Uni <82521845+luke-Uni@users.noreply.github.com>
2023-01-19 14:38:53 +0100

    network and type speed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+function getConnection() {
+  return (
+    navigator.connection ||
+    navigator.mozConnection ||
+    navigator.webkitConnection ||
+    navigator.msConnection
+  );
+}
+
+function updateNetworkInfo(info) {
+  document.getElementById("networkType").innerHTML = info.type;
+  document.getElementById("effectiveNetworkType").innerHTML =
+    info.effectiveType;
+  document.getElementById("downlinkMax").innerHTML = info.downlinkMax;
+}
+
+var info = getConnection();
+if (info) {
+  info.onchange = function (event) {
+    updateNetworkInfo(event.target);
+  };
+  updateNetworkInfo(info);
+}
 document.getElementById("status").innerHTML = navigator.onLine
   ? "online"
   : "offline";
